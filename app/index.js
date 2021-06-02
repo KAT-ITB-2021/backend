90e430244d1ce8084c2d15d001f0da79c94b70d2
Fransiskus Febryan Suryawan <franfebryan@protonmail.com>
2021-06-02 20:10:07 +0700

    Separate app and entry point

--- app/index.js
+++ app/index.js
@@ -1,10 +1,5 @@
-const express = require('express');
+const app = require('./app');
 const config = require('./config');
-
-const db = require('./database/models');
-
-const app = express();
-app.use('/api', require('./routes/login'));
 
 app.listen(config.express.port, config.express.ip);
 console.log(`Server started at http://${config.express.ip}:${config.express.port}`);
