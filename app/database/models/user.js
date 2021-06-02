79bfc09ef2ade8adb1b17abc9bd1bdae9ea1978d
Fransiskus Febryan Suryawan <franfebryan@protonmail.com>
2021-06-02 18:50:48 +0700

    Use sequelize cli

--- app/database/models/user.js
+++ app/database/models/user.js
@@ -1,17 +1,25 @@
-module.exports = (sequelize, Sequelize) => {
-  const User = sequelize.define('user', {
-    username: {
-      type: Sequelize.STRING
-    },
-    hashedPassword: {
-      type: Sequelize.STRING
-    },
-    role: {
-      type: Sequelize.STRING
-    },
-    salt: {
-      type: Sequelize.STRING
+'use strict';
+const {
+  Model
+} = require('sequelize');
+module.exports = (sequelize, DataTypes) => {
+  class User extends Model {
+    /**
+     * Helper method for defining associations.
+     * This method is not a part of Sequelize lifecycle.
+     * The `models/index` file will call this method automatically.
+     */
+    static associate(models) {
+      // define association here
     }
+  };
+  User.init({
+    username: DataTypes.STRING,
+    hashedPassword: DataTypes.STRING,
+    salt: DataTypes.STRING
+  }, {
+    sequelize,
+    modelName: 'User',
   });
   return User;
-}
+};
--- app/index.js
+++ app/index.js
@@ -2,13 +2,9 @@ const express = require('express');
 const config = require('./config');
 
 const db = require('./database/models');
-await db.sequelize.sync();
 
 const app = express();
 app.use('/api', require('./routes/login'));
-app.get('/', (req, res) => {
-  res.send('Hello, world!');
-});
 
 app.listen(config.express.port, config.express.ip);
 console.log(`Server started at http://${config.express.ip}:${config.express.port}`);
