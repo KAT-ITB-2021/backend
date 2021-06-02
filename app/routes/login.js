ca9dd9569d325db2701ec385abcef592be5667c4
Fransiskus Febryan Suryawan <franfebryan@protonmail.com>
2021-06-03 00:33:52 +0700

    Fix linter

--- app/routes/login.js
+++ app/routes/login.js
@@ -1,10 +1,6 @@
 const express = require('express');
 const router = new express.Router();
-// const jwt = require('jsonwebtoken');
-const formidable = require('formidable');
 const { login } = require('../controllers/userController');
-
-const ParseURLEncoded = express.urlencoded({extended: false});
 
 router.post('/login', login);
 
