6b878788d5e591a72094857d62441470da3ae81c
Fransiskus Febryan Suryawan <franfebryan@protonmail.com>
2021-06-18 01:29:00 +0700

    Add helper for authentication and roles

--- app/helper/auth.js
+++ app/helper/auth.js
@@ -1,19 +1,41 @@
 const jwt = require('jsonwebtoken');
 const { jwtSecret } = require('../config');
+const { ROLES } = require('./constants');
 
 module.exports = {
   /**
-   * Membaca token JWT, verifikasi, lalu mengembalikan objek berisi
-   * nama dan role dari user yang mengirim JWT
-   * @param {String} token token JWT
-   * @returns {{username: String, role: String}} objek user, keduanya berisi string kosong jika JWT tidak valid
+   * Fungsi wrapper untuk membuat JWT dari payload yang ada dan expiration time-nya
+   * @param {Object} payload payload dari JWT
+   * @param {String} exp waktu expirasi dari JWT
    */
-  parseToken(token){
-    try{
-      return jwt.verify(token, jwtSecret);
+  generateToken(payload, exp){
+    return jwt.sign(payload, jwtSecret, { expiresIn: exp });
+  },
+  /**
+   * Middleware untuk parse token, hasilnya dimasukkan dalam objek
+   * `userToken` pada req
+   */
+  parseToken(req, res, next){
+    const authHeader = req.headers.authorization;
+    if(authHeader){
+      const token = authHeader.split(' ')[1];
+      try{
+        req.userToken = jwt.verify(token, jwtSecret);
+        next();
+      }
+      catch(err){
+        res.status(403).json({message: 'bad token'});
+      }
     }
-    catch(err){
-      return {username: '', role: ''};
+    else{
+      res.status(401).send({message: 'unauthorized'});
     }
+  },
+  /**
+   * Middleware untuk otentikasi role dengan kuasa "edit" konten
+   */
+  authEdit(req, res, next){
+    if(req.userToken.role === ROLES.pendikpus) next();
+    else res.status(403).send();
   }
 };
