f33b7132c0dd1f736afcfb667d0a589cc87695c9
Fransiskus Febryan Suryawan <franfebryan@protonmail.com>
2021-06-19 10:50:33 +0700

    Add Tugas CRUD

--- app/app.js
+++ app/app.js
@@ -4,5 +4,6 @@ const app = express();
 app.use('/', require('./routes/login'));
 app.use('/', require('./routes/register'));
 app.use('/', require('./routes/materi'));
+app.use('/', require('./routes/tugas'));
 
 module.exports = app;
