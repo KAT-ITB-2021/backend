e7f98b97005edf0f67a31685acc18551f8e4a97d
Fransiskus Febryan Suryawan <franfebryan@protonmail.com>
2021-06-20 13:10:34 +0700

    Add CORS

--- app/app.js
+++ app/app.js
@@ -1,6 +1,8 @@
 const express = require('express');
+const cors = require('cors');
 
 const app = express();
+app.use(cors());
 app.use('/', require('./routes/login'));
 app.use('/', require('./routes/register'));
 app.use('/', require('./routes/materi'));
