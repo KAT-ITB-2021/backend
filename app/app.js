56725e5738a10caa6a642e987de66dd7d086142f
Fransiskus Febryan Suryawan <franfebryan@protonmail.com>
2021-06-23 19:23:43 +0700

    Add routes to app

--- app/app.js
+++ app/app.js
@@ -7,5 +7,7 @@ app.use('/', require('./routes/login'));
 app.use('/', require('./routes/register'));
 app.use('/', require('./routes/materi'));
 app.use('/', require('./routes/tugas'));
+app.use('/', require('./routes/webinar'));
+app.use('/', require('./routes/presensi'));
 
 module.exports = app;
