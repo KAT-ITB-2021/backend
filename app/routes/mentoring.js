f21974a45676d9cd764b65fd2c89b239a9d71a51
Fransiskus Febryan Suryawan <franfebryan@protonmail.com>
2021-06-24 17:09:31 +0700

    Add extra routes for mentoring and webinar

--- app/routes/mentoring.js
+++ app/routes/mentoring.js
@@ -2,7 +2,7 @@ const express = require('express');
 const router = new express.Router();
 const { parseToken, authEdit } = require('../helper/auth');
 const { addMentoring, getLatestMentoring, editMentoring, removeMentoring, getAllMentoring } = require('../controllers/mentoringController');
-const { addMentoringDetail, editMentoringDetail, removeMentoringDetail, getAllMentoringDetails, getOneMentoringDetail, getAssociatedMentorings } = require('../controllers/mentoringDetailController');
+const { addDetailMentoring, editDetailMentoring, removeDetailMentoring, getAllDetailMentorings, getOneDetailMentoring, getAssociatedMentorings } = require('../controllers/mentoringDetailController');
 
 router.post('/mentoring*', parseToken);
 router.post('/mentoring/now', getLatestMentoring);
@@ -13,11 +13,11 @@ router.post('/mentoring/admin/remove/:id', removeMentoring);
 router.post('/mentoring/admin/getall', getAllMentoring);
 
 router.post('/mentoringdetail*', parseToken, authEdit);
-router.post('/mentoringdetail/add', addMentoringDetail);
-router.post('/mentoringdetail/edit/:id', editMentoringDetail);
-router.post('/mentoringdetail/remove/:id', removeMentoringDetail);
-router.post('/mentoringdetail/getall', getAllMentoringDetails);
-router.post('/mentoringdetail/get/:id', getOneMentoringDetail);
+router.post('/mentoringdetail/add', addDetailMentoring);
+router.post('/mentoringdetail/edit/:id', editDetailMentoring);
+router.post('/mentoringdetail/remove/:id', removeDetailMentoring);
+router.post('/mentoringdetail/getall', getAllDetailMentorings);
+router.post('/mentoringdetail/get/:id', getOneDetailMentoring);
 router.post('/mentoringdetail/mentorings', getAssociatedMentorings);
 
 module.exports = router;
