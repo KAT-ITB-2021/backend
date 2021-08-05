0810ae79d29c3869940035b2bc3065f32a77c2e6
Josep Marcello <jspmarcello@live.com>
2021-08-06 05:37:58 +0700

    chore: migrate user to prisma

--- app/controllers/userController.js
+++ app/controllers/userController.js
@@ -1,61 +1,70 @@
-const { User } = require('../database/models');
 const { generateToken } = require('../helper/auth');
 const crypto = require('crypto');
 const { ROLES } = require('../helper/constants');
 const { parseForm } = require('../helper/parseform');
+const prisma = require('../helper/prisma');
 
 const CURRENT_ROLE = ROLES.peserta;
 
 module.exports = {
   async register(req, res, next) {
-    /* Handle Register */
     try{
       const { fields } = await parseForm(req);
-      if(!fields.email || !fields.password) return res.status(400).json({ message: 'No email or password provided'});
-      if(!fields.name || !fields.nim) return res.status(400).json({ message: 'No name or nim provided'});
+      if(!fields.email || !fields.password)
+        return res.status(400).json({ message: 'No email or password provided'});
+      if(!fields.name || !fields.nim)
+        return res.status(400).json({ message: 'No name or nim provided'});
       const salt = crypto.randomBytes(32);
-      crypto.pbkdf2(fields.password, salt, 50000, 64, 'sha512', (_, derivedKey) => {
-        User.create({
-          nama: fields.name,
-          nim: fields.nim,
-          email: fields.email,
-          kelompok: fields.kelompok,
-          hashedPassword: derivedKey.toString('hex'),
-          salt: salt.toString('hex'),
-          role: CURRENT_ROLE
-        }).then((user) => {
+
+      const now = new Date();
+
+      crypto.pbkdf2(fields.password, salt, 50000, 64, 'sha512', async (_, derivedKey) => {
+        try {
+          const newUser = await prisma.users.create({
+            data: {
+              nama: fields.name,
+              nim: fields.nim,
+              email: fields.email,
+              kelompok: fields.kelompok,
+              hashedPassword: derivedKey.toString('hex'),
+              salt: salt.toString('hex'),
+              role: CURRENT_ROLE,
+              createdAt: now,
+              updatedAt: now,
+            }
+          });
           res.status(200).send({
             token: generateToken({
-              id: user.id,
-              nama: user.nama,
-              email: user.email,
-              nim: user.nim,
-              kelompok: user.kelompok,
-              role: user.role
+              id: newUser.id,
+              nama: newUser.nama,
+              email: newUser.email,
+              nim: newUser.nim,
+              kelompok: newUser.kelompok,
+              role: newUser.role
             }, '1d')
           });
-        }).catch((err) => {
-          console.log(err);
-          res.status(400).json({message: 'User already exists'});
-        });
+        } catch (e) {
+          console.log(e);
+          res.status(400).json({ message: 'User already exists' });
+        }
       });
-    }
-    catch(err){
+    } catch(err) {
       next(err);
     }
   },
 
-  async login(req, res, next){
+  async login(req, res, next) {
     try{
       const { fields } = await parseForm(req);
       if(!fields.email || !fields.password) return res.status(400).json({ message: 'Login fail' });
-      const user = await User.findOne({where: { email: fields.email }});
+
+      const user = await prisma.users.findUnique({ where: { email: fields.email } });
+
       if(user === null){
         res.status(400).json({ message: 'Invalid user or password' });
-      }
-      else{
+      } else {
         crypto.pbkdf2(fields.password, Buffer.from(user.salt, 'hex'), 50000, 64, 'sha512', (_, derivedKey) => {
-          if(derivedKey.toString('hex') === user.hashedPassword){
+          if (derivedKey.toString('hex') === user.hashedPassword) {
             res.status(200).json({
               token: generateToken({
                 id: user.id,
@@ -66,51 +75,54 @@ module.exports = {
                 kelompok: user.kelompok
               }, '1d')
             });
-          }
-          else{
+          } else {
             res.status(400).json({ message: 'Invalid user or password' });
           }
         });
       }
-    }
-    catch(err){
+    } catch(err) {
       next(err);
     }
   },
 
-  async changePassword(req, res){
+  async changePassword(req, res) {
     try{
       const { fields } = await parseForm(req);
       const { oldPassword, newPassword } = fields;
       const { id } = req.userToken;
-      const user = await User.findOne({
+      const user = await prisma.users.findUnique({
         where: { id }
       });
-      if(!user.id) res.status(400).json({message: 'user invalid'});
-      else{
+
+      if(!user.id) res.status(400).json({ message: 'user invalid' });
+      else {
         const salt = Buffer.from(user.salt, 'hex');
-        console.log(oldPassword);
         const hashedOld = crypto.pbkdf2Sync(oldPassword, salt, 50000, 64, 'sha512');
-        if(hashedOld.toString('hex') === user.hashedPassword){
+
+        if (hashedOld.toString('hex') === user.hashedPassword) {
           const newSalt = crypto.randomBytes(32);
           const hashedNew = crypto.pbkdf2Sync(newPassword, newSalt, 50000, 64, 'sha512');
-          user.salt = newSalt.toString('hex');
-          user.hashedPassword = hashedNew.toString('hex');
-          try{
-            await user.save();
+
+          try {
+            await prisma.users.update({
+              where: {
+                id: user.id,
+              },
+              data: {
+                hashedPassword: hashedNew.toString('hex'),
+                salt: newSalt.toString('hex'),
+              },
+            });
             res.json({message: 'success updating password'});
-          }
-          catch(err){
+          } catch(err) {
             console.log(err);
             res.status(500).json({message: 'error updating password'});
           }
-        }
-        else{
+        } else {
           res.status(400).json({message: 'password invalid'});
         }
       }
-    }
-    catch(err){
+    } catch(err) {
       res.status(400).json({message: 'error parsing form'});
     }
   }
