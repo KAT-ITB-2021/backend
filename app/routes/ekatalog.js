2dafa02e8fe84bc7ca136a8f31e3d87011737d00
averus9 <16520480@std.stei.itb.ac.id>
2021-08-08 01:14:47 +0700

    router

---
+++ app/routes/ekatalog.js
@@ -0,0 +1,18 @@
+const express = require('express');
+const router = new express.Router();
+const { parseToken, authEdit } = require('../helper/auth');
+const { addProduk, addSponsor, getAllProduk, getAllSponsor, getProdukById, getSponsorById, removeProduk, removeSponsor } = require('../controllers/ekatalogController');
+
+router.post('/ekatalog*', parseToken);
+router.get('/ekatalog/produk/getall', getAllProduk);
+router.get('/ekatalog/produk/:id', getProdukById);
+router.get('/ekatalog/sponsor/getall', getAllSponsor);
+router.get('/ekatalog/sponsor/:id', getSponsorById);
+router.post('/ekatalog/admin*', authEdit);
+router.post('/ekatalog/admin/sponsor/add', addSponsor);
+router.post('/ekatalog/admin/produk/add', addProduk);
+router.delete('/ekatalog/admin/sponsor/remove/:id', removeSponsor);
+router.delete('/ekatalog/admin/produk/remove/:id', removeProduk);
+
+
+module.exports = router;
