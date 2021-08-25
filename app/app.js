005615892190862fdbeb1b50f7bb7cec65cea92b
Josep Marcello <57025993+jspmarc@users.noreply.github.com>
2021-08-25 18:47:22 +0700

    Revert "Back end OHK"

--- app/app.js
+++ app/app.js
@@ -13,6 +13,5 @@ app.use('/', require('./routes/presensi'));
 app.use('/', require('./routes/mentoring'));
 app.use('/', require('./routes/ekatalog'));
 app.use('/', require('./routes/tamuGaleri'));
-app.use('/', require('./routes/ohk'));
 
 module.exports = app;
